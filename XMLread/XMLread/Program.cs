9f5587dcf90d3e0cfad2f40d169240cbc252cff1
user <user@2-3>
2017-03-17 12:27:02 +0800

    123

--- XMLread/XMLread/Program.cs
+++ XMLread/XMLread/Program.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Xml.Linq;
 namespace XMLread
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<library> librarys = new List<library>();
+
+            var xml = XElement.Load("file.xml");
+            var librarysnode = xml.Descendants("Info").ToList();
+
+            for(var i = 0; i < librarysnode.Count(); i++)
+            {
+                var librarynode = librarysnode[i];
+            }
+            foreach(var librarynode in librarysnode)
+            {
+                var groupTypeName = librarynode.Attribute("groupTypeName").Value.Trim();
+                var email = librarynode.Attribute("email").Value.Trim();
+                var fax = librarynode.Attribute("fax").Value.Trim();
+                library librarydata = new library();
+                librarydata.groupTypeName = groupTypeName;
+                librarydata.email = email;
+                librarydata.fax = fax;
+                Console.WriteLine(groupTypeName);
+                Console.WriteLine(email);
+                Console.WriteLine(fax);
+            }
+            librarysnode
+                .Where(x => !x.IsEmpty).ToList()
+                .ForEach(librarynode => { });
+            Console.ReadLine();
         }
     }
 }
---
+++ XMLread/XMLread/library.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLread
+{
+    class library
+    {
+        public string groupTypeName;
+        public string mainTypeName;
+        public string mainTypePK;
+        public string name;
+        public string name_eng;
+        public string typename;
+        public string level;
+        public string representImage;
+        public string intro;
+        public string intro_eng;
+        public string areaCode;
+        public string cityName;
+        public string address;
+        public string longitude;
+        public string latitude;
+        public string openTime;
+        public string openTime_eng;
+        public string ticketPrice;
+        public string ticketPrice_eng;
+        public string contact;
+        public string phone;
+        public string fax;
+        public string email;
+        public string website;
+        public string srcWebsite;
+    }
+}
