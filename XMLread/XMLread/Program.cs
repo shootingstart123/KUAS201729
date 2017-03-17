a2c88b904661c20b9456a7c278c667a964e1a148
shootingstart123 <1103105329@gm.kuas.edu.tw>
2017-03-17 10:06:00 +0800

    Add files via upload

---
+++ XMLread/XMLread/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
