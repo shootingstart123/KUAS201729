761ba0d40feefc2acbf4ab8563d04ce7089f7839
shootingstart123 <1103105329@gm.kuas.edu.tw>
2017-03-19 19:27:29 +0800

    2017319

--- XMLread/XMLread/Program.cs
+++ XMLread/XMLread/Program.cs
@@ -19,18 +19,27 @@ namespace XMLread
             {
                 var librarynode = librarysnode[i];
             }
+            int count = 0;
             foreach(var librarynode in librarysnode)
             {
-                var groupTypeName = librarynode.Attribute("groupTypeName").Value.Trim();
-                var email = librarynode.Attribute("email").Value.Trim();
+                ++count;
+                var name = librarynode.Attribute("name").Value.Trim();
+                var address = librarynode.Attribute("address").Value.Trim();
+                var openTime = librarynode.Attribute("openTime").Value.Trim();
+                var phone = librarynode.Attribute("phone").Value.Trim();
                 var fax = librarynode.Attribute("fax").Value.Trim();
                 library librarydata = new library();
-                librarydata.groupTypeName = groupTypeName;
-                librarydata.email = email;
+                librarydata.name = name;
+                librarydata.address = address;
+                librarydata.openTime = openTime;
+                librarydata.phone = phone;
                 librarydata.fax = fax;
-                Console.WriteLine(groupTypeName);
-                Console.WriteLine(email);
-                Console.WriteLine(fax);
+                Console.WriteLine("第"+count+"筆"+"資料");
+                Console.WriteLine("\t圖書館名:"+name);
+                Console.WriteLine("\t住址:"+address);
+                Console.WriteLine("\t開館時間:"+openTime);
+                Console.WriteLine("\t聯絡電話:"+phone);
+                Console.WriteLine("\t傳真:"+fax);
             }
             librarysnode
                 .Where(x => !x.IsEmpty).ToList()
