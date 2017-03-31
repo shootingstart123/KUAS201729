468869e5368b9be7d93d801c529189d5684a07ab
shootingstart123 <1103105329@gm.kuas.edu.tw>
2017-03-31 12:20:07 +0800

    3/31 新增寫入讀取XML資料

--- XMLread/XMLread/Program.cs
+++ XMLread/XMLread/Program.cs
@@ -8,19 +8,27 @@ namespace XMLread
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             List<library> librarys = new List<library>();
+            var obj = new Database();
+            //CreatDatabase(obj);
+            ShoeDatabase(obj);
+            Console.ReadKey();
+        }
+        public static void CreatDatabase(Database d)
+        {
 
             var xml = XElement.Load("file.xml");
             var librarysnode = xml.Descendants("Info").ToList();
 
-            for(var i = 0; i < librarysnode.Count(); i++)
+
+            for (var i = 0; i < librarysnode.Count(); i++)
             {
                 var librarynode = librarysnode[i];
             }
             int count = 0;
-            foreach(var librarynode in librarysnode)
+            foreach (var librarynode in librarysnode)
             {
                 ++count;
                 var name = librarynode.Attribute("name").Value.Trim();
@@ -34,17 +42,28 @@ namespace XMLread
                 librarydata.openTime = openTime;
                 librarydata.phone = phone;
                 librarydata.fax = fax;
-                Console.WriteLine("第"+count+"筆"+"資料");
-                Console.WriteLine("\t圖書館名:"+name);
-                Console.WriteLine("\t住址:"+address);
-                Console.WriteLine("\t開館時間:"+openTime);
-                Console.WriteLine("\t聯絡電話:"+phone);
-                Console.WriteLine("\t傳真:"+fax);
+                d.CreateLibrary(librarydata);
+
             }
             librarysnode
                 .Where(x => !x.IsEmpty).ToList()
-                .ForEach(librarynode => { }); 
-            Console.ReadLine();
+                .ForEach(librarynode => { });
+
+        }
+        public static void ShoeDatabase(Database d)
+        {
+            List<library> libraryget = new List<library>();
+            libraryget=d.ReadLibrary();
+            int count = 1;
+            libraryget.ForEach(x => {
+                Console.WriteLine("第" + (count++) + "筆" + "資料");
+                Console.WriteLine("\t圖書館名:" + x.name);
+                Console.WriteLine("\t住址:" + x.address);
+                Console.WriteLine("\t開館時間:" + x.openTime);
+                Console.WriteLine("\t聯絡電話:" + x.phone);
+                Console.WriteLine("\t傳真:" + x.fax);
+                Console.WriteLine();
+            });
         }
     }
 }
