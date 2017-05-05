45af1cb8c680b1f408b8778537f7e69368449740
shootingstart123 <1103105329@gm.kuas.edu.tw>
2017-05-05 12:24:30 +0800

    新增View顯示及砍掉多餘的專案

--- XMLread/web/Controllers/DemoController.cs
+++ XMLread/web/Controllers/DemoController.cs
@@ -9,7 +9,7 @@ namespace web.Controllers
     public class DemoController : Controller
     {
         // GET: Demo
-        public ActionResult Index()
+        public ActionResult Index_Message()
         {
             var lib_database = new Database();
             var libs = lib_database.ReadLibrary();
@@ -17,10 +17,22 @@ namespace web.Controllers
             libs.ForEach(x =>
             {
                 message += string.Format("圖書館名：{0}<br/>住址:{1}<br/>開館時間：{2}<br/>聯絡電話:{3}<br/>傳真:{4}<br/><br/>", x.name, x.address,x.openTime,x.phone,x.fax);
-
             });
             return Content(message);
-
         }
+        public ActionResult Index()
+        {
+            var lib_database = new Database();
+            var libs = lib_database.ReadLibrary();
+            //var message = string.Format("已收到{0}筆圖書館的資料<br/><br/>", libs.Count);
+            //libs.ForEach(x =>
+            //{
+            //    message += string.Format("圖書館名：{0}<br/>住址:{1}<br/>開館時間：{2}<br/>聯絡電話:{3}<br/>傳真:{4}<br/><br/>", x.name, x.address, x.openTime, x.phone, x.fax);
+            //});
+            ViewBag.UserName = "Lin";
+            ViewBag.libs = libs;
+            return View(libs);
+        }
+
     }
 }
